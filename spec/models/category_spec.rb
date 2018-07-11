996c68e46b24efb5bb7b33bdbc1613e10d798356
Siianko <tsiyanko@gmail.com>
2018-07-11 12:21:28 +0200

    created validation test for the presence of title

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :title }
+  end
+
+  describe 'Validations' do 
+    it { is_expected.to validate_presence_of :title }
+  end
+    
 end
