cac13eeea3fa1d71067e8f5edb60b3050bde5682
Siianko <tsiyanko@gmail.com>
2018-07-11 16:13:52 +0200

    Add factory to categories

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -8,5 +8,10 @@ RSpec.describe Category, type: :model do
   describe 'Validations' do 
     it { is_expected.to validate_presence_of :title }
   end
-    
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryBot.create(:category)).to be_valid
+    end
+  end
 end
